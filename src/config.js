60f2a90c6b98400f7ecc293dc5fe5ee68fde8cd3
Abdulqodir Tolibov <78257822+Abdulqodir-coder@users.noreply.github.com>
2021-08-15 19:06:04 +0500

    Last changes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import os from 'os'
 const host = 'localhost'
 // const host = os.networkInterfaces()['Wi-Fi'].find(wifi => wifi.family === 'IPv4' && wifi.internal === false).address
-const PORT = 4500
+const PORT = process.env.PORT || 4500
 const PRIVATE_KEY = 'PRIVATE'
 
 export {
